4cab2e4f274237a7296bd528fc7b831f1df8c51b
Nawakorn23 <nawakorn.c@gmail.com>
2024-05-13 01:20:51 +0700

    add vercel.json

--- Backend/server.js
+++ Backend/server.js
@@ -62,7 +62,7 @@ app.use("/api/librarys", librarys);
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
-  console.log(`Server runing in`, process.env.NODE_ENV, `mode on port`, PORT)
+  console.log(`Server runing in`, process.env.NODE_ENV, "on " + process.env.HOST + ":" + PORT)
 );
 
 // Handle unhandle promise rejections
